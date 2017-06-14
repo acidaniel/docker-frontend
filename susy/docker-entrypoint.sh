67a2e5d465b5a5178d203efbc772a447390bb342
Daniel Serna <acidaniel@gmail.com>
2017-06-14 02:14:38 -0500

    Solving issues with npm link and refactoring entrypoint by adding ENV

--- susy/docker-entrypoint.sh
+++ susy/docker-entrypoint.sh
@@ -2,38 +2,7 @@
 
  #prevent conflicts with existing
 
-# rm -rf node_modules
-
- /usr/bin/npm link \
-   babel-plugin-transform-remove-strict-mode \
-   babel-preset-es2015 \
-   bower \
-   breakpoint-sass \
-   browser-sync \
-   chroma-sass \
-   del \
-   font-awesome \
-   gulp-eslint \
-   gulp-autoprefixer \
-   gulp-babel \
-   gulp-cssnano \
-   gulp-compass \
-   gulp-concat \
-   gulp-imagemin \
-   gulp-notify \
-   gulp-order \
-   gulp-rename \
-   gulp-sass-lint \
-   gulp-size \
-   gulp-sourcemaps \
-   gulp-uglify \
-   kss \
-   gulp-kss \
-   node-normalize-scss \
-   node-sass-import-once \
-   run-sequence \
-   support-for \
-   susy \
-   singularitygs 
+ rm -rf node_modules
+ /usr/bin/npm link gulp gulp-sass $NPM_PACKAGES
 
  exec "$@"
