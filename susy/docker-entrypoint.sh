3510c49cd02e36a2db806f1993388dfdc91a523d
Daniel Serna <acidaniel@gmail.com>
2017-06-15 09:13:27 -0500

    Adding dependencies and solving conflicts on libraries.

--- susy/docker-entrypoint.sh
+++ susy/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
- #prevent conflicts with existing
+# prevent conflicts with existing
+rm -rf node_modules
 
- rm -rf node_modules
- /usr/bin/npm link gulp gulp-sass $NPM_PACKAGES
+/usr/bin/npm link gulp gulp-sass $NPM_PACKAGES
 
- exec "$@"
+exec "$@"
